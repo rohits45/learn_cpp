d970be0524b89681fac66ff2b5d9e54c2f778cde
Rohit Shendage <67877157+rohits45@users.noreply.github.com>
2021-09-19 18:49:49 +0530

    simple calculator using CPP for +-*/

---
+++ calculator.cpp
@@ -0,0 +1,68 @@
+//simple calculator usin CPP for +-*/
+#include<iostream>
+using namespace std;
+
+
+int main()
+{   
+    int a,b;
+    char ch='y',op;
+    cout<<"**** welcome to cpp calcultor **** \n **you can perform** \nAddition    subtraction    multiplication    divide\n";
+ 
+    while(ch=='y'||ch=='Y')
+    {
+        cout<<"Enter two Oprands:\n";
+        cin>>a>>b;
+        cout<<"\nEnter Oprator: ";
+        cin>>op;
+    switch(op)
+    {
+
+    case '+' :
+        cout<<"Ans=: "<<a+b;
+        
+        cout<<"\nEnter Y or y to Continue: ";
+        cin>>ch;
+    break;
+
+    case '-' :
+        cout<<"Ans=: "<<a-b;
+        
+        cout<<"\nEnter Y or y to Continue: ";
+        cin>>ch;
+    break;
+    
+    case '*' :
+        cout<<"Ans=: "<<a*b;
+        
+        cout<<"\nEnter Y or y to Continue: ";
+        cin>>ch;
+    break;
+
+    case '/' :
+        if(b==0)
+        {
+            cout<<"\n****Divide by 0 error****\n";
+            continue;
+        }
+        float c;
+        c=float(a)/b;
+        cout<<"Floating point Ans=: "<<c;
+        cout<<"\nDivison=: "<<a/b;
+        cout<<"\tRemainder=: "<<a%b;
+        cout<<"\nEnter Y or y to Continue: ";
+        cin>>ch;
+    break;
+
+    default:
+        cout<<"Wrong Choice";
+        
+        cout<<"\nEnter Y or y to Continue: ";
+        cin>>ch;
+        exit(0);
+    break;
+
+    }
+    }
+    return 0;
+}
