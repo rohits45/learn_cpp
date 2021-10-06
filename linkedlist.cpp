185fbb24c44479f629a96a2656238af3ae95b216
Rohit Shendage <67877157+rohits45@users.noreply.github.com>
2021-10-06 15:06:40 +0530

    created a linked list

---
+++ linkedlist.cpp
@@ -0,0 +1,37 @@
+#include<iostream>
+using namespace std;
+
+struct node{
+    int data;
+    node *next;
+}
+
+class linked_list{
+    private:
+        node *head,*tail;
+    public:
+        linked_list(){
+            head = NULL;
+            tail = NULL;
+        }
+    void add_node(int n){
+        node *tmp = new node;
+        tmp -> data = n;
+        tmp -> next = NULL;
+
+        if(head == NULL){
+            head = tmp;
+            tail = tmp;
+        }
+        else{
+            tail -> next = tmp;
+            tail = tail -> next;
+        }
+    }
+
+};
+
+int main(){
+
+    return 0;
+}
