8ed335559830f761c61473cfb157603d8daccc64
Rohit Shendage <67877157+rohits45@users.noreply.github.com>
2021-10-17 19:31:43 +0530

    2 ways to write the factorial program
    Factorial Program using loop
    Factorial Program using recursion

--- factorial.cpp
+++ factorial.cpp
@@ -1,16 +1,27 @@
+//Factorial Program in C++: Factorial of n is the product of all positive descending integers.
+// Factorial of n is denoted by n!.
+// Eg. 6! = 6*5*4*3*2*1 = 720
 #include<iostream>
 using namespace std;
 
 class fact{
+    long long int ans=1;
+
     public:
-    int factorial(int n){
+    int factorial(int n){   //Factorial Program using Recursion
         if(n==1)
             return 1;
         else
             return n * factorial(n-1);
     }
- };
+    int factorial_normal(int m){    //Factorial Program using Loop
+        for(int i = 1; i <= m; i++){
+            ans *= i; 
+        }
+        return ans;
+    }
 
+ };
 
 int main()
 {
@@ -18,7 +29,7 @@ int main()
     fact f;
     cout<<"Enter Number :-";
     cin>>n;
-    cout<<f.factorial(n);
-
+    cout<<"Factorial using Loop for number "<<n<<" :- "<<f.factorial_normal(n)<<endl;
+    cout<<"Factorial using Recursion for number "<<n<<" :- "<<f.factorial(n)<<endl;
 
 }
